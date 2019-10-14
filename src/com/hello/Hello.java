d91079a77361058a369f21746d2b9b793fd3ef86
YT <YT@192.168.7.63>
2019-10-14 12:15:06 +0800

    修改了源代码,同步代码git上面的代码

--- src/com/hello/Hello.java
+++ src/com/hello/Hello.java
@@ -3,5 +3,6 @@ package com.hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("传到git");
+        System.out.println("修改了源代码,同步代码git上面的代码");
     }
 }
